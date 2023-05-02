377afdfc986ac43547f4d4e9786b72f51f5a201e
wpwcbc <43486791+wpwcbc@users.noreply.github.com>
2023-05-02 15:52:21 +0800

    Get ip in console when a device connected

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+	cors: {
+	  origin: 'http://localhost:3000',
+	  methods: ['GET', 'POST'],
+	  allowedHeaders: ['my-custom-header'],
+	  credentials: true
+	}
+  });
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+io.on('connection', (socket) => {
+	console.log('User connected:', socket.id);
+
+	// Fetch the public IP address of the server using an IIFE with an async function
+	(async () => {
+		try {
+		  const response = await axios.get('https://api.ipify.org?format=json');
+		  const ipAddress = response.data.ip;
+		  console.log('Server IP Address:', ipAddress);
+		} catch (error) {
+		  console.error('Failed to fetch IP address:', error);
+		}
+	  })();
+  
+	// Custom event for broadcasting the IP address of a connected peer
+	socket.on('broadcast-ip', (ip) => {
+	  socket.broadcast.emit('peer-ip', ip);
+	});
+  
+	socket.on('disconnect', () => {
+	  console.log('User disconnected:', socket.id);
+	});
+});
--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,35 @@ import AudioPlayer from './AudioScript/AudioPlayer';
 // Addictional decoration 
 import './index.css';
 
-// App() function are the main function of the webpage
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+
+import axios from 'axios';
+
+
+const socket = io('http://localhost:3001');
+
 export default function App() {
+  useEffect(() => {
+    // Fetch the client's public IP address
+  const fetchIPAddress = async () => {
+    try {
+      const response = await axios.get('https://api.ipify.org?format=json');
+      const ipAddress = response.data.ip;
+      // Send the IP address to the server
+      socket.emit('broadcast-ip', ipAddress);
+    } catch (error) {
+      console.error('Failed to fetch IP address:', error);
+    }
+  };
+
+  fetchIPAddress();
+
+  socket.on('peer-ip', (ip) => {
+    console.log('Received peer IP:', ip);
+  });
+  }, []);
+
   return (
     // React Router
     <div id="mainApp">
